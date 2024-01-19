8d9cb7a83c4430ee625a967c131239107646be62
palakthakur341 <154214777+palakthakur341@users.noreply.github.com>
2024-01-19 22:11:45 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+// import React,{ Component } from "react";
+// import './App.css';
+// import {tictactoe} from './components/tictactoe/tictactoe';
+
+// function App() {
+//   return (
+//     <div>
+//       <tictactoe/>
+//       hello;
+//     </div>
+//   );
+// }
+
+// export default App;
+import './App.css';
+import TicTacToe from './components/tictactoe/tictactoe';
+
+function App() {
+  return (
+    <div>
+      <TicTacToe/>
+      hello;
+    </div>
+  );
+}
+
+export default App;
